18a6a1548fcdd469629626633eadfb7253ea74f1
Jun Liang <illuminatemynight@gmail.com>
2020-03-21 08:46:01 -0500

    revised solution.js

--- multiples-of-3-or-5/solution.js
+++ multiples-of-3-or-5/solution.js
@@ -1,17 +1,12 @@
 function solution(n){
     var sum = 0;
-    var arr = [];
     
-    for(let i = 0; i < n; i++) {
-      if(i%3 == 0) {
-        arr.push(i);
+    for(var i = 0; i < n; i++) {
+    
+      if(i%3 == 0 || i%5 == 0) {
         sum += i;
       }
       
-      else if (i%5 == 0) {
-        arr.push(i);
-        sum += i;
-      }
     }
     
     return sum; 
